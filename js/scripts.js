b504dcd4ac688f3a86477f20bd1a9bb22fcc3b71
rickanutyi <erachynybaev@gmail.com>
2022-03-07 18:06:26 +1300

    f

--- js/scripts.js
+++ js/scripts.js
@@ -51,12 +51,163 @@ function startPlay(round, audio, play) {
     }
   }
 }
+//white theme
+
+const sun = document.getElementById("sun");
+const leftCard = document.getElementById("left__card");
+
+// sun.onclick = function () {
+//   leftCard.style.border = "3px solid red";
+//   leftCard.style.animationName = "whiteBorder";
+//   leftCard.style.animationDuration = "2s";
+//   leftCard.style.animationFillMode = "forwards";
+// };
 
 // sections
 const section1 = document.getElementById("section1");
 const section2 = document.getElementById("section2");
 const section3 = document.getElementById("section3");
 const section4 = document.getElementById("section4");
+// section 1 canvas
+
+// !wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+(function () {
+  let canvas = document.createElement("canvas");
+  let ctx = canvas.getContext("2d");
+  let w = (canvas.width = innerWidth);
+  let h = (canvas.height = innerHeight);
+
+  let particles = [];
+  let properties = {
+    bgColor: "rgba(17,17,17,1)",
+    particleColor: "rgba(255,255,0,1)",
+    particleRadius: 3,
+    particleCount: 100,
+    particleMaxVelocity: 0.5,
+    lineLength: 150,
+    particleLife: 6,
+  };
+
+  setTimeout(() => {
+    properties.particleColor = "rgba(255,40,40,1)";
+  }, 10000);
+
+  document.getElementById("right__side").appendChild(canvas);
+
+  window.onresize = function () {
+    w = canvas.width = innerWidth;
+    h = canvas.height = innerHeight;
+  };
+
+  class Particle {
+    constructor() {
+      this.x = Math.random() * w;
+      this.y = Math.random() * h;
+      this.velocityX =
+        Math.random() * (properties.particleMaxVelocity * 2) -
+        properties.particleMaxVelocity;
+      this.velocityY =
+        Math.random() * (properties.particleMaxVelocity * 2) -
+        properties.particleMaxVelocity;
+      this.life = Math.random() * properties.particleLife * 60;
+    }
+
+    position() {
+      //x
+      (this.x + this.velocityX > w && this.velocityX > 0) ||
+      (this.x + this.velocityX < 0 && this.velocityX < 0)
+        ? (this.velocityX *= -1)
+        : this.velocityX;
+      //y
+      (this.y + this.velocityY > h && this.velocityY > 0) ||
+      (this.y + this.velocityY < 0 && this.velocityY < 0)
+        ? (this.velocityY *= -1)
+        : this.velocityY;
+      //
+      this.x += this.velocityX;
+      this.y += this.velocityY;
+    }
+
+    reDraw() {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, properties.particleRadius, 0, Math.PI * 2);
+      ctx.closePath();
+      ctx.fillStyle = properties.particleColor;
+      ctx.fill();
+    }
+
+    recalcLife() {
+      if (this.life < 1) {
+        this.x = Math.random() * w;
+        this.y = Math.random() * h;
+        this.velocityX =
+          Math.random() * (properties.particleMaxVelocity * 2) -
+          properties.particleMaxVelocity;
+        this.velocityY =
+          Math.random() * (properties.particleMaxVelocity * 2) -
+          properties.particleMaxVelocity;
+        this.life = Math.random() * properties.particleLife * 60;
+      }
+      this.life--;
+    }
+  }
+
+  function reDrowBackground() {
+    ctx.fillStyle = properties.bgColor;
+    ctx.fillRect(0, 0, w, h);
+  }
+
+  function drawLines() {
+    let x1, y1, x2, y2, length, opacity;
+    for (let i in particles) {
+      for (let j in particles) {
+        x1 = particles[i].x;
+        y1 = particles[i].y;
+        x2 = particles[j].x;
+        y2 = particles[j].y;
+
+        length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        if (length < properties.lineLength) {
+          opacity = 1 - length / properties.lineLength;
+          ctx.lineWidth = "0.5";
+          ctx.strokeStyle = `rgba(255,255,255,${opacity})`;
+          ctx.beginPath();
+          ctx.moveTo(x1, y1);
+          ctx.lineTo(x2, y2);
+          ctx.closePath();
+          ctx.stroke();
+        }
+      }
+    }
+  }
+
+  function reDrowParticles() {
+    for (let i in particles) {
+      particles[i].recalcLife();
+      particles[i].position();
+      particles[i].reDraw();
+    }
+  }
+
+  function loop() {
+    reDrowBackground();
+    reDrowParticles();
+    drawLines();
+    requestAnimationFrame(loop);
+  }
+
+  function init() {
+    for (let i = 0; i < properties.particleCount; i++) {
+      particles.push(new Particle());
+    }
+    loop();
+  }
+
+  init();
+})();
+
+// !ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
 
 section1.onwheel = function (event) {
   if (event.deltaY > 0) {
